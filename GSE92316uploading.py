c1338dbaf0dd41e0606ffedccc04f373be08e63f
Thomas Yu <thomasyu888@gmail.com>
2018-07-23 13:39:10 -0700

    Add in annotation script

---
+++ GSE92316uploading.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import synapseclient
+import synapseutils
+syn = synapseclient.login()
+
+GSE = "GSE92316"
+gseMetadata = "%s-geo-metadata.tsv" % GSE
+parentId = "syn12976510"
+
+## Read in the metadata extracted from GEO above.
+tsv = pd.read_csv(gseMetadata, sep="\t")
+
+## Extract the GEO accession number and URL from the GEO metadata.
+newTsv = tsv[["geo_accession", "url"]]
+
+## Handle requirements of sync_manifest.py:
+## (1) the column holding the URL must be called path to be recognized by sync_manifest.py
+## (2) synapseStore should be FALSE to indicate we are linking to URLs rather than uploading files
+## (3) parent should be the Synapse ID of the container in Synapse that will hold the links.  Here we will
+##     use a container called bulk-upload-annotation (Synapse ID: syn10002942)
+## (4) used should indicate any input data used to derive the annotations.  We will ignore by setting to NA.
+## (5) executed should indicate any scripts/binaries/etc used to derive the annotations.  We will ignore by setting to NA.
+newTsv.columns = ["geo_accession", "path"]
+newTsv['synapseStore'] = "FALSE"
+newTsv['parent'] = parentId
+newTsv['used'] = float('nan')
+newTsv['executed'] = float('nan')
+
+
+newTsv['tumorType'] = 
+
+newTsv['assay'] = 
+
+
+newTsv['dataSubtype'] = "raw" 
+newTsv['dataType'] = 
+newTsv['fileFormat'] = "raw"
+newTsv['resourceType'] = "experimentalData"
+
+newTsv['cellType'] = 
+newTsv['diagnosis'] = 
+
+newTsv['isCellLine'] = False
+newTsv['isPrimaryCell'] = True #Coming from human / animal
+
+newTsv['tissue'] = "mammary gland"
+newTsv['organ'] = "breast"
+
+
+newTsv['species'] = "Human"
+newTsv['sex'] = float('nan')
+#Some specimens could come from one individual
+newTsv['specimenID'] = tsv['title']
+newTsv['individualID'] = 
+
+newTsv['platform'] = "HiSeq3000" #??
+
+newTsv['consortium'] = "CSBC"
+newTsv['fundingAgency'] = "NIH-NCI"
+
+
+## Output the re-annotated data
+newTsv.to_csv("%s-manifest.tsv" % GSE, sep="\t", index=False)
+
+
+synapseutils.syncToSynapse(syn, "%s-manifest.tsv" % GSE)
