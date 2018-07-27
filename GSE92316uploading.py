226b0f3baff8dcfe97935e121e1d4cf5f9742942
Thomas Yu <thomasyu888@gmail.com>
2018-07-27 14:16:15 -0700

    Edits for GSE92316

--- GSE92316uploading.py
+++ GSE92316uploading.py
@@ -27,17 +27,17 @@ newTsv['used'] = float('nan')
 newTsv['executed'] = float('nan')
 
 
-newTsv['tumorType'] = 
+newTsv['tumorType'] = "Adenocarcinoma"
 
 newTsv['assay'] = "rnaSeq"
 
 
 newTsv['dataSubtype'] = "raw" 
-newTsv['dataType'] = 
+newTsv['dataType'] = "geneExpression"
 newTsv['fileFormat'] = "raw"
 newTsv['resourceType'] = "experimentalData"
 
-newTsv['cellType'] = 
+newTsv['cellType'] = "epithelial"
 newTsv['diagnosis'] = "Breast Cancer"
 
 newTsv['isCellLine'] = False
@@ -58,7 +58,56 @@ newTsv['platform'] = "HiSeq3000" #??
 newTsv['consortium'] = "CSBC"
 newTsv['fundingAgency'] = "NIH-NCI"
 
+##### ADD supplemental file
 
+supp = tsv[["geo_accession", "supplementary_file_1"]]
+
+## Handle requirements of sync_manifest.py:
+## (1) the column holding the URL must be called path to be recognized by sync_manifest.py
+## (2) synapseStore should be FALSE to indicate we are linking to URLs rather than uploading files
+## (3) parent should be the Synapse ID of the container in Synapse that will hold the links.  Here we will
+##     use a container called bulk-upload-annotation (Synapse ID: syn10002942)
+## (4) used should indicate any input data used to derive the annotations.  We will ignore by setting to NA.
+## (5) executed should indicate any scripts/binaries/etc used to derive the annotations.  We will ignore by setting to NA.
+supp.columns = ["geo_accession", "path"]
+supp['synapseStore'] = "FALSE"
+supp['parent'] = parentId
+supp['used'] = float('nan')
+supp['executed'] = float('nan')
+
+
+supp['tumorType'] = "Adenocarcinoma"
+
+supp['assay'] = "rnaSeq"
+
+
+supp['dataSubtype'] = "processed" 
+supp['dataType'] = "geneExpression"
+supp['fileFormat'] = "tsv"
+supp['resourceType'] = "experimentalData"
+
+supp['cellType'] = "epithelial"
+supp['diagnosis'] = "Breast Cancer"
+
+supp['isCellLine'] = False
+supp['isPrimaryCell'] = True #Coming from human / animal
+
+supp['tissue'] = "mammary gland"
+supp['organ'] = "breast"
+
+
+supp['species'] = "Human"
+supp['sex'] = float('nan')
+#Some specimens could come from one individual
+supp['specimenID'] = tsv['title']
+supp['individualID'] = [spec.split(" ")[0] for spec in tsv['title']]
+
+supp['platform'] = "HiSeq3000" #??
+
+supp['consortium'] = "CSBC"
+supp['fundingAgency'] = "NIH-NCI"
+
+newTsv = newTsv.append(supp)
 ## Output the re-annotated data
 newTsv.to_csv("%s-manifest.tsv" % GSE, sep="\t", index=False)
 
